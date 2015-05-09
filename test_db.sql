2e1076698e17cfe04fe7f91569c1c8526d72a2a1
rochevin <vincent.rocher@etu.univ-rouen.fr>
2015-05-09 21:21:50 +0200

    reverse engineer pour le schema de la base et modification d'erreurs rencontrés, creation du fichier .sql

--- test_db.sql
+++ test_db.sql
@@ -1,38 +1,12 @@
-<h1 style="text-align: center;">Projet Base de données</h1>
-__Auteurs :__ Stéphanie LEVON & Vincent ROCHER
+CREATE DATABASE gestion_prescription CHARACTER SET 'utf8';
 
-#Introduction
-##Sujet 
-Le but de ce projet est de gérer des examens demandés par des prescripteurs. Un examen consistera en l'étude, pour un patient donné, d'un panel de gènes et produira un rapport/compte-rendu clinique.
-##Schéma de la base
-![schéma_base](schema_base.jpg)
-#Création des tables
-##Table `type_personnel`
-###Description
-type_personnel_id | type_personnel_nom 
-:-----: | :------
-INT | VARCHAR
-1 | Administrateur
-2 | Préscripteur
-3 | Clinicien
+USE gestion_prescription;
 
-
-###Création de la table
-``` sql
 CREATE TABLE type_personnel (
     type_personnel_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     type_personnel_nom VARCHAR(60) NOT NULL
 )
-ENGINE=InnoDB;          
-```
-
-##Table `personnel`
-###Description
-personnel_id | personnel_nom | personnel_prenom | personnel_mail | personnel_password | personnel_type_personnel_id
-:-----: | :------: | :------: | :------: | :------: | :------ 
-INT | VARCHAR | VARCHAR | VARCHAR | CHAR(SHA1) | INT
-
-``` sql
+ENGINE=InnoDB;   
 CREATE TABLE personnel (
     personnel_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     personnel_prenom VARCHAR(60) NOT NULL,
@@ -47,17 +21,7 @@ CREATE TABLE personnel (
         FOREIGN KEY (personnel_type_personnel_id) 
         REFERENCES type_personnel(type_personnel_id)
 )
-ENGINE=InnoDB DEFAULT CHARSET=utf8;          
-```
-## Table `patient`
-###Description
-patient_id | patient_num_secu | patient_prenom | patient_nom | patient_mail | patient_sexe | patient_ethnie | patient_date_naissance | patient_num_tel | patient_pere_id | patient_mere_id | patient_prescripteur_id | patient_clinicien_id
-:-----: | :------: | :-----: | :-----: | :-----: | :-----: | :-----: | :-----: | :-----: | :-----: | :-----: | :-----: | :-----
-INT | INT | VARCHAR | VARCHAR | VARCHAR | VARCHAR | VARCHAR | DATE | VARCHAR | INT | INT | INT | INT
-
-
-###Création de la table
-``` sql
+ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 CREATE TABLE patient (
     patient_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     patient_num_secu INT UNSIGNED NOT NULL,
@@ -86,54 +50,18 @@ CREATE TABLE patient (
         FOREIGN KEY (patient_clinicien_id) 
         REFERENCES personnel(personnel_id)   
 )
-ENGINE=InnoDB;          
-```
-
-##Table `panel_gene`
-###Description
-panel_gene_id | panel_gene_nom
-:-----: | :------ 
-INT | VARCHAR
-
-
-
-###Création de la table
-``` sql
+ENGINE=InnoDB;  
 CREATE TABLE panel_gene (
     panel_gene_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     panel_gene_nom VARCHAR(60) NOT NULL
 )
-ENGINE=InnoDB;          
-```
-
-##Table `gene`
-###Description
-gene_id | gene_nom | gene_chromosome
-:-----: | :-----: | :------ 
-INT | VARCHAR | CHAR
-
-
-
-###Création de la table
-``` sql
+ENGINE=InnoDB;
 CREATE TABLE gene (
     gene_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     gene_nom VARCHAR(60) NOT NULL,
     gene_chromosome CHAR(2) NOT NULL
 )
-ENGINE=InnoDB;          
-```
-
-##Table `assoc_panel_gene`
-###Description
-assoc_gene_id | assoc_panel_id
-:-----: | :------ 
-INT | INT
-
-
-
-###Création de la table
-``` sql
+ENGINE=InnoDB;   
 CREATE TABLE assoc_panel_gene(
     assoc_gene_id INT NOT NULL,
     assoc_panel_id INT NOT NULL,
@@ -143,19 +71,7 @@ CREATE TABLE assoc_panel_gene(
     CONSTRAINT fk_assoc_panel_id FOREIGN KEY (assoc_panel_id)
         REFERENCES panel_gene(panel_gene_id)
 )
-ENGINE=InnoDB;          
-```
-
-##Table `examen`
-###Description
-examen_id | examen_nom | examen_date | examen_patient_id | examen_panel_gene_id
-:-----: | :-----: | :-----: | :-----: | :------ 
-INT | VARCHAR | DATE | INT | INT
-
-
-
-###Création de la table
-``` sql
+ENGINE=InnoDB; 
 CREATE TABLE examen (
     examen_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     examen_nom VARCHAR(60) NOT NULL,
@@ -169,19 +85,7 @@ CREATE TABLE examen (
         FOREIGN KEY (examen_panel_gene_id) 
         REFERENCES panel_gene(panel_gene_id)
 )
-ENGINE=InnoDB;          
-```
-
-##Table `rapport`
-###Description
-rapport_id | rapport_nom | rapport_date | rapport_pathologie | rapport_examen_id | rapport_prescripteur_id
-:-----: | :-----: | :-----: | :-----: | :-----: | :------ 
-INT | VARCHAR | DATE | VARCHAR | INT | INT
-
-
-
-###Création de la table
-``` sql
+ENGINE=InnoDB;  
 CREATE TABLE rapport (
     rapport_id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     rapport_nom VARCHAR(60) NOT NULL,
@@ -196,5 +100,4 @@ CREATE TABLE rapport (
         FOREIGN KEY (rapport_prescripteur_id) 
         REFERENCES personnel(personnel_id)
 )
-ENGINE=InnoDB;          
-```
+ENGINE=InnoDB;   
